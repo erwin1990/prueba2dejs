dbfdd436b4de8b92500253026c529e3ffc701de3
erwin1990 <camperwin@hotmail.com>
2019-05-27 19:52:58 -0300

    se modifica estrutura de las funciones para que salgan los alert

--- js/javascript.js
+++ js/javascript.js
@@ -37,38 +37,41 @@ function leerJSON(json) {
   document.getElementById('tabla').innerHTML = out;
 }
 
-//funcion que saca el promedio de las 10 nmotas
-function Promedio(json) {
-  var total =0;
-  for (var i = 0; i < json.length; i++) {
+//funcion que saca el rpmedio de los 10 estudiantes
+function promedios(Estudiantes) {
+  var json = estudiantes;
+  var total = 0;
+  for (i = 0; i < json.length; i++) {
     total += json[i].nota;
   }
-  var nota = (total/10).toFixed(2);
-    document.getElementById('tabla').innerHTML = "La nota promedio de los Estudiantes es: " + nota;
+  var nota = (total/10).toFixed(2)
+  alert ("El promedio de los 10 estudiantes es: " + nota);
 }
 
-//funcion que saca al estudiantes con mayor nota
-function notaMayor(json) {
-    var notamayor = 0;
-    var pos = 0;
-    for (i = 0; i < json.length; i++) {
-        if (json[i].nota > notamayor) {
-            notamayor = json[i].nota;
-            pos = i;
-        }
-    }
-    document.getElementById("tabla").innerHTML = "El estudiantes: " + json[pos].nombre + " saco la mayor nota: " + notamayor;
-  }
 
-//funcion que saca al estudiantes con mayor nota
-function notaMenor(json) {
-    var notamenor = json[0].nota;
-    var pos = 0;
-    for (i = 0; i < json.length; i++) {
-        if (json[i].nota < notamenor) {
-            notamenor = json[i].nota;
-            pos = i;
-        }
+//funcion que lee la propiedad nota y de vuelve la nota mayor
+function calcularNotaMayor(Estudiantes) {
+  var json = estudiantes;
+  var notamayor = 0;
+  for (i = 0; i < json.length; i++) {
+    if (json[i].nota > notamayor) {
+      notamayor = json[i].nota;
+      pos =i;
     }
-    document.getElementById("tabla").innerHTML = "El estudiantes: " + json[pos].nombre + " saco la menor nota: " + notamenor;
   }
+  alert ("El estudiante: " + json[pos].nombre + " saco la mayor nota: " + notamayor);
+}
+
+
+//funcion que lee la propiedad nota y de vuelve la nota menor
+function calcularNotaMenor(Estudiantes) {
+  var json = estudiantes;
+  var notamenor = json[0].nota;
+    for (i = 0; i < json.length; i++) {
+      if (json[i].nota < notamenor ) {
+        notamenor = json[i].nota;
+        pos =i;
+      }
+    }
+  alert ("El estudiante: " + json[pos].nombre + " saco la menor nota: " + notamenor);
+}
